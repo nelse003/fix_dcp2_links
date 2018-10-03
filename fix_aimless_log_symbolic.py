35eaf4119dbf93334f89b53a30967728f33b6320
Elliot Nelson <elliot.nelson@dtc.ox.ac.uk>
2018-10-03 11:22:27 +0100

    basic loop and detection of borken symlinks

---
+++ fix_aimless_log_symbolic.py
@@ -0,0 +1,16 @@
+# Check whether symbolic link fails
+def symlink_fail(path):
+if not os.path.exists(os.readlink(path)):
+    print 'path %s is a broken symlink' % path
+
+return os.path.exists(os.readlink(path))
+
+# Loop over all DCP2 symbolic links
+
+ini_model_path = "/dls/labxchem/data/2016/lb13385-64/processing/analysis/initial_model"
+
+for folder in os.listdir(ini_model_path):
+
+    print(folder)
+
+# Write link to Database
